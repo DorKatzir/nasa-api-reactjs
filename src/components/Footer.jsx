10de0587504a27062ee90c672558e6fba76eaa7f
Dor Katzir <katzirdor@gmail.com>
2024-04-30 12:03:27 +0300

    project demo content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
         <footer>
-            
+            <div>
+                <h2>Dynamic title for the current image</h2>
+                <h1>APOD PROJECT</h1>
+            </div>
+            <button>
+                info
+            </button>
         </footer>
     )
 }
