edd9ce8940bbaf5138897f4195457f51bebacaa7
Dor Katzir <katzirdor@gmail.com>
2024-05-01 08:49:19 +0300

    creating api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Main from './components/Main'
 import SideBar from './components/SideBar';
 import Footer from './components/Footer';
 
 export default function App() {
 
-    const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
     const  [showModal, setShowModal] = useState(false)
-
+    
     function handleToggleModal(){
         setShowModal(!showModal)
     }
+    
+    useEffect(()=>{
+        
+        async function fetchApiData() {
+            const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+            const url = 'https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
+            try{
+                const res = await fetch(url)
+                const data = res.json()
+                // console.log('DATA\n', data)
+                console.log(data)
 
+            }catch(err){
+                console.log(err.message)
+            }
+        }
+
+        fetchApiData()
+
+    },[])
+
+    
     return (
         <>
             <Main />
