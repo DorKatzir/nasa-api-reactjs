19fa5862dccfc244bf900e08669d0c96b8af6317
Dor Katzir <katzirdor@gmail.com>
2024-05-19 22:46:56 +0300

    video player

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,22 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
+import ReactPlayer from 'react-player'
+
 export default function Main({ data }) {
     
     return (
 
+
         <div className="imgContainer">
 
             {
-                data.media_type === 'image' 
-                ?  <img className='bgImage' src={ data?.hdurl } alt={ data.title || 'bg-image' } />
-
-                // :  <video  width='100%' height='100%' controls autoPlay={true} muted playsInline >
-                //     <source src={data?.url} type='video/mp4'/>
-                // </video>
-                : <video controls>
-                    {/* <source src={ data?.url } type="video/webm" /> */}
-                    <source src={ data?.url } />
-                    <p>
-                        Your browser does not support HTML video. Here is a
-                        <a href={ data?.url }>link to the video</a> instead.
-                    </p>
-                </video>
-
+                data?.media_type === 'image' && <img className='bgImage' src={ data?.hdurl } alt={ data.title || 'bg-image' } />
             }
-            
+
+            {
+                data?.media_type === 'video' && <ReactPlayer className='bgVideo' url={ data?.url} controls={true} />
+            }
+  
         </div>
 
     )
