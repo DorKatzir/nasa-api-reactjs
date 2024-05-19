10b053477a82647f428e88efb021605db2d64b34
Dor Katzir <katzirdor@gmail.com>
2024-05-19 21:02:41 +0300

    video fetch

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
 export default function App() {
 
     const [data, setData] = useState(null) 
-    const [cach, setCash] = useState(null)
     const  [showModal, setShowModal] = useState(false)
     
     function handleToggleModal(){
@@ -19,32 +18,25 @@ export default function App() {
     
     useEffect(()=>{
         
-        const url = 'https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
+        const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`
 
         async function fetchApiData() {
 
             const today = (new Date()).toDateString()
             const localKey = `NASA-${today}`
+
             if (localStorage.getItem(localKey)){
                 const apiData = JSON.parse(localStorage.getItem(localKey))
                 setData(apiData)
-                console.log('Fetched from todays Cach')
+                console.log('Fetched from Local storage')
                 return
             }
-
-            setCash(data)
             localStorage.clear()
 
             try{
 
                 const res = await fetch(url)
                 const apiData = await res.json()
-                
-                if(apiData.media_type !== 'image'){
-                    setData(cach)
-                    console.log('Fetched from yesturdays Cach because no video display avaliable')
-                    return
-                }
 
                 localStorage.setItem(localKey, JSON.stringify(apiData))
                 setData(apiData)
