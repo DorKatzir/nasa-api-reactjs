9082b0d7afbf2749ef15838b1c2ee88b91f8ab59
Dor Katzir <katzirdor@gmail.com>
2024-05-19 22:25:12 +0300

    installing react video player

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 export default function Main({ data }) {
     
     return (
 
         <div className="imgContainer">
-            <img className="bgImage" src={ data?.hdurl } alt={ data.title || 'bg-image' } />
+
+            {
+                data.media_type === 'image' 
+                ?  <img className='bgImage' src={ data?.hdurl } alt={ data.title || 'bg-image' } />
+
+                // :  <video  width='100%' height='100%' controls autoPlay={true} muted playsInline >
+                //     <source src={data?.url} type='video/mp4'/>
+                // </video>
+                : <video controls>
+                    {/* <source src={ data?.url } type="video/webm" /> */}
+                    <source src={ data?.url } />
+                    <p>
+                        Your browser does not support HTML video. Here is a
+                        <a href={ data?.url }>link to the video</a> instead.
+                    </p>
+                </video>
+
+            }
+            
         </div>
 
     )
