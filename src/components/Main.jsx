bc2ee297e524e83a59f4995a36850de67b88cc17
Dor Katzir <katzirdor@gmail.com>
2024-05-19 23:56:22 +0300

    ReactPlayer responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,19 +6,36 @@ import ReactPlayer from 'react-player/youtube'
 export default function Main({ data }) {
     
     return (
-
-
-        <div className="imgContainer">
-
+        <>
+        
             {
-                data?.media_type === 'image' && <img className='bgImage' src={ data?.hdurl } alt={ data.title || 'bg-image' } />
-            }
+                data?.media_type === 'image' &&
 
-            {
-                data?.media_type === 'video' && <ReactPlayer url={ data?.url} playsinline={true} controls={false} light={true} muted={true} width='100%' height='100%'/>
+                    <div className="imgContainer">
+                        <img className='bgImage' src={ data?.hdurl } alt={ data.title || 'bg-image' } />
+                    </div>
             }
-  
-        </div>
+        
+        
+            {
+                data?.media_type === 'video' && 
+                    <div className='player-wrapper'>
+                        <ReactPlayer
+                            className='react-player'
+                            url={ data?.url }
+                            width='100%'
+                            height='100%'
+                            playsinline={true}
+                        />
+                    </div>
+            }
+        
+        </>
+
+
+
+        
 
     )
+
 }
